1096617e2fb83ffe75161be3b4193502a1ff94f3
TenJeno <jenokalinszki@tengroup.com>
2022-09-14 11:50:12 +0100

    made product properties nullable

--- DockerCarvedRockApi/ApiModels/Product.cs
+++ DockerCarvedRockApi/ApiModels/Product.cs
@@ -3,9 +3,9 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; } 
+        public string? Name { get; set; }
+        public string? Description { get; set; } 
         public double Price { get; set; }
-        public string Category { get; set; }
+        public string? Category { get; set; }
     }
 }
